f741d58838e724d75f1e3a951a436a47afa0f680
montse-tecnocampus <157364202+montse-tecnocampus@users.noreply.github.com>
2026-09-10 12:41:07 +0200

    Updated Step 5

--- src/multipleSteps/Step_5_Final.java
+++ src/multipleSteps/Step_5_Final.java
@@ -8,32 +8,17 @@ public class Step_5_Final {
 	{
 		JConsole console = new JConsole(80,20);
 		
-		int number;
-		int times;
+		//TODO: Declare your variables here
 
 		console.println ("Step 5. THE END. DECOMPOSITION IN PRIME FACTORS");
 		console.println ("---------------------------------------------");
 		console.println ();
 
-		number = read ("Number that will be factorized", 2, 199999, console);
-
-		console.println ();
-
-		if (isPrimeNumber (number)) {
-			console.println (number+" is itself a prime number!");
-		} 
-		else {
-			console.println (number+" is decomposed in prime numbers in the following way:");
-			console.println ();
-			for (int i = 2; i <= number/2; i++) {
-				if (isPrimeNumber (i)) {
-					times = timesItCanBeDivided (number, i);
-					if (times != 0) {
-						console.println ("   "+i+" to the power of "+times);
-					}
-				}
-			}
-		}
+		// TODO: COMPLETE
+		// If something can be done using the available functions,
+		// then it MUST be done using those functions.
+		
+		
 		console.setCursorPosition (0, console.getRows() - 1);
 		console.print ("Press any key to exit");
 		console.readKey (true);
@@ -41,67 +26,5 @@ public class Step_5_Final {
 
 	}
 
-	public static int divisorsPropis (int n) {
-		int q = 1;
-
-		for (int i = 2; i <= n/2; i++) {
-			if (n%i==0) {
-				q++;
-			}
-		}
-		return q;
-	}
-
-	static int countProperDivisors (int n) {
-
-		int q = 1;
-
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (n%i==0) {
-				q++;
-			}
-		}
-		return q;
-	}
-
-	public static boolean isPrimeNumber (int n) {
-		return countProperDivisors(n) == 1;
-	}
-
-	public static int timesItCanBeDivided (int number, int candidate) {
-		int times, remainer;
-
-		times = 0;
-		remainer = number % candidate;
-		while (remainer == 0) {
-			// si som aqui el residu és zero
-			times++;
-			number = number / candidate;
-			remainer = number % candidate;
-		}
-		// en arribar a aquest punt el residu de la divisió és
-		// diferent de zero i això vol dir que ja no es pot continuar dividint
-
-		return times;
-
-	}
-
-	public static int read (String msg, int min, int max, JConsole console) {
-		int number;
-
-		console.print (msg+" ["+min+","+max+"]: ");
-		console.setForegroundColor(Color.GREEN);
-		number = console.readInt();
-
-		while (!(number>=min && number<=max)) {
-			console.setForegroundColor(Color.RED);
-			console.println ("   Number out of bounds. It must be within range "+min+" and "+max);
-			console.resetColor ();
-			console.print (msg+" ["+min+","+max+"]: ");
-			console.setForegroundColor(Color.GREEN);
-			number = console.readInt();
-		}
-		console.resetColor ();
-		return number;
-	}
+	/*TODO: Copy here the functions created in the previous steps*/
 }
